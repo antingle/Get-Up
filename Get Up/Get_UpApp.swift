d2cd5cb2687f077649b8cb6b9a41cacd3559c58d
Anthony Ingle <ingleanthony@icloud.com>
2021-02-13 12:21:32 -0500

    Initial Commit

---
+++ Get Up/Get_UpApp.swift
@@ -0,0 +1,17 @@
+//
+//  Get_UpApp.swift
+//  Get Up
+//
+//  Created by Anthony on 2/13/21.
+//
+
+import SwiftUI
+
+@main
+struct Get_UpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
